9db5030246e3646d9c03fe9739eed5d08cc34eee
anas-1995 <anasalazmeh.95@gmail.com>
2018-12-16 14:32:53 +0200

    feedback8

--- src/app/core/services/call-api.service.ts
+++ src/app/core/services/call-api.service.ts
@@ -15,7 +15,8 @@ export class CallApiService {
   constructor(public http: HttpClient, private loginSer: LoginService) {
   }
   // readonly baseUrl = "http://104.217.253.15:2999/api/"
-  readonly baseUrl = "http://178.62.233.91:3000/api/"
+  // readonly baseUrl = "http://178.62.233.91:3000/api/"
+  readonly baseUrl = "http://azzyimmigration.com:3000/api/"
   // readonly baseUrl = "http://192.168.1.9:3000/api/"
   private errorCode = 0;
   private code = "";
